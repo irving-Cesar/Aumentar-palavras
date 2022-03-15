08d92031d2572eac9e5b1cec722bda55463e4622
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-03-14 22:22:33 -0300

    Melhorando css

--- js/JsMaior.js
+++ js/JsMaior.js
@@ -26,7 +26,6 @@ function aumentar() {
                 img.style.width = "100px";
                 img.style.height = "100px";
                 imagens.appendChild(img);
-
             }else{
                 var img = document.createElement("img");
                 img.src = "img/Teste_Espaco.jpg";
@@ -38,91 +37,11 @@ function aumentar() {
 
         }
 
-        switch (TManho[j]) {
-            case 'a':
-                mostrarC(TManho[j]);
-                break;
-            case 'b':
-                mostrarC(TManho[j]);
-                break;
-            case 'c':
-                mostrarC(TManho[j]);
-                break;
-            case 'd':
-                mostrarC(TManho[j]);
-                break;    
-            case 'e':
-                mostrarC(TManho[j]);
-                break;
-            case 'f':
-                mostrarC(TManho[j]);
-                break;
-            case 'g':
-                mostrarC(TManho[j]);
-                break;
-            case 'h':
-                mostrarC(TManho[j]);
-                break;
-            case 'i':
-                mostrarC(TManho[j]);
-                break;
-            case 'j':
-                mostrarC(TManho[j]);
-                break;
-            case 'k':
-                mostrarC(TManho[j]);
-                break;
-            case 'l':
-                mostrarC(TManho[j]);
-                break;
-            case 'm':
-                mostrarC(TManho[j]);
-                break;
-            case 'n':
-                mostrarC(TManho[j]);
-                break;
-            case 'o':
-                mostrarC(TManho[j]);
-                break;
-            case 'p':
-                mostrarC(TManho[j]);
-                break;
-            case 'q':
-                mostrarC(TManho[j]);
-                break;
-            case 'r':
-                mostrarC(TManho[j]);
-                break;
-            case 's':
-                mostrarC(TManho[j]);
-                break;
-            case 't':
-                mostrarC(TManho[j]);
-                break;
-            case 'u':
-                mostrarC(TManho[j]);
-                break;
-            case 'v':
-                mostrarC(TManho[j]);
-                break;
-            case 'w':
-                mostrarC(TManho[j]);
-                break;
-            case 'x':
-                mostrarC(TManho[j]);
-                break;
-            case 'y':
-                mostrarC(TManho[j]);
-                break;
-            case 'z':
-                mostrarC(TManho[j]);
-                break;
-            case ' ':
-                mostrarC(TManho[j]);
-                break;
-            default:
-                break;    
+
+        for (const n of TManho) {
+            mostrarC(n)
         }
+
         texto.value = '';
     }
 
