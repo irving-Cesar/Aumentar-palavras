bc791700027359b1c49fdbde04f5cb60a8b303a0
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-31 22:39:12 -0300

    Add files via upload

--- js/JsMaior.js
+++ js/JsMaior.js
@@ -1,8 +1,9 @@
 var imagens = document.getElementById("imgs");
 var contador = 0;
 
-function aumentar() {
 
+function aumentar() {
+    // imagens.style.display = "block";
     var texto = document.getElementById("nome");
     var TManho = [] // Array para armazenar caracter
 
@@ -13,38 +14,45 @@ function aumentar() {
         }
     }
 
-    TManho.length > 0? imagens.style.borderStyle = "groove" : imagens.style.borderStyle = "none";
+    if (TManho.length > 0){
+        imagens.style.borderStyle = "groove";
+    } else{
+        alert('Insira algum caracter.')
+    }
+    
+    function mostrarC(value){
 
-    for (var j = 0; j <= TManho.length; j++) {
-                
-        function mostrarC(value){
-            if (value != ' ') {
-                var CM = value.toUpperCase();
-                var img = document.createElement("img");
-                img.src = "img/Teste_Letra_"+CM+".jpg";
-                img.style.borderRadius = "6px";
-                img.style.width = "100px";
-                img.style.height = "100px";
-                imagens.appendChild(img);
-            }else{
-                var img = document.createElement("img");
-                img.src = "img/Teste_Espaco.jpg";
-                img.style.borderRadius = "5px";
-                img.style.width = "50px";
-                img.style.height = "100px";
-                imagens.appendChild(img);
-            }
+        if (value != ' ') {
+            var CM = value.toUpperCase();
+            var img = document.createElement("img");
+            img.src = "img/Teste_Letra_"+CM+".jpg";
+            img.style.borderRadius = "6px";
+            img.style.width = "100px";
+            img.style.height = "100px";
+            imagens.appendChild(img);
 
+        }else{
+            var img = document.createElement("img");
+            img.src = "img/Teste_Espaco.jpg";
+            img.style.borderRadius = "5px";
+            img.style.width = "50px";
+            img.style.height = "100px";
+            imagens.appendChild(img);
         }
 
-
-        for (const n of TManho) {
-            mostrarC(n)
-        }
-
-        texto.value = '';
     }
 
+    for (const n of TManho) {
+        mostrarC(n)
+    }
+
+    texto.value = '';
+    
 }
 
-
+function clean () {
+    while (imagens.firstChild) {
+        imagens.removeChild(imagens.firstChild)
+    }
+    imagens.style.display = "none";
+}
